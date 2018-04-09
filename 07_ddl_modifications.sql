9a255723928a0d7e7dbc6d68500cb1e8cde9eed3
Gabriel Wong <gabrielwong1988@gmail.com>
2018-04-09 17:38:36 +0800

    Update DDL Modifications workshop

--- 07_ddl_modifications.sql
+++ 07_ddl_modifications.sql
@@ -1,109 +1,100 @@
--- Question 1
-CREATE TABLE MemberCategories
-	(MemberCategory nvarchar(2),
-	MemberCatDescription nvarchar(200)
-	
-	PRIMARY KEY(MemberCategory)
-	)
+/* 1. Create a Table called MemberCategories with the following fields
+MemberCategory nvarchar(2) MemberCatDescription nvarchar(200)
+None of the fields above can be null. Set the MemberCategory as the Primary key. */
+CREATE TABLE MemberCategories (
+MemberCategory			nvarchar(2)		NOT NULL,
+MemberCatDescription	nvarchar(200)	NOT NULL
+PRIMARY KEY (MemberCategory)
+)
 
-DROP TABLE MemberCategories
-
--- Question 2
+/* 2. Add the following data into the MemberCategories Table... */
 INSERT INTO MemberCategories
-VALUES ('A', 'Class A Members')
+VALUES
+('A', 'Class A Members'),
+('B', 'Class B Members'),
+('C', 'Class C Members')
 
-INSERT INTO MemberCategories
-VALUES ('B', 'Class B Members')
+/* 3. Create a Table called GoodCustomers with the following fields... */
+CREATE TABLE GoodCustomers (
+CustomerName		nvarchar(50)	NOT NULL,
+Address				nvarchar(65),
+PhoneNumber			nvarchar(9)		NOT NULL,
+MemberCategory		nvarchar(2),
+PRIMARY KEY(CustomerName, PhoneNumber),
+FOREIGN KEY(MemberCategory) REFERENCES MemberCategories (MemberCategory)
+)
 
-INSERT INTO MemberCategories
-VALUES ('C', 'Class C Members')
-
--- Question 3
-CREATE TABLE GoodCustomers
-	(
-	CustomerName nvarchar(50) not null,
-	Address nvarchar(65),
-	PhoneNumber nvarchar(9) not null,
-	MemberCategory nvarchar(2),
-	PRIMARY KEY (CustomerName, PhoneNumber),
-	FOREIGN KEY (MemberCategory) REFERENCES MemberCategories(MemberCategory)
-	)
-
--- Question 4
+/* 4. Insert into GoodCustomer all records form Customer table with corresponding fields except Address, which is to be left Null. Only Customers having Member Category ‘A’ or ‘B’ are good customers hence the table should be inserted only those records from the Customers table. */
 INSERT INTO GoodCustomers
-	(CustomerName, PhoneNumber, MemberCategory)
-	SELECT CustomerName, PhoneNumber, MemberCategory
-	FROM Customers
-	WHERE MemberCategory IN ('A','B')
+(CustomerName, PhoneNumber, MemberCategory)
+SELECT CustomerName, PhoneNumber, MemberCategory
+FROM Customers
+WHERE MemberCategory IN ('A', 'B')
 
--- Question 5
+/* 5. Insert into GoodCustomers the following new customer... */
 INSERT INTO GoodCustomers
-	(CustomerName, PhoneNumber, MemberCategory)
-	VALUES ('Tracy Tan', 736572, 'B')
+(CustomerName, PhoneNumber, MemberCategory)
+VALUES
+('Tracy Tan', 736572, 'B')
 
--- Question 6
+/* 6. Insert into GoodCustomers table the following information for a new customer */
 INSERT INTO GoodCustomers
-VALUES ('Grace Leong', '15 Bukit Purmei Road, Singapore 0904', 278865, 'A')
+VALUES
+('Grace Leong', '15 Bukit Purmei Road, Singapore 0904', 278865, 'A')
 
--- Question 7
+/* 7. Insert into GoodCustomers table the following information for a new customer. This should not go through! */
 INSERT INTO GoodCustomers
-VALUES ('Lynn Lim', '15 Bukit Purmei Road, Singapore 0904', 278865, 'P')
+VALUES
+('Lynn Lim', '15 Bukit Purmei Road, Singapore 0904', 278865, 'P')
 
--- Question 8
+/* 8. Change the Address of Grace Leong so that the new address is '22 Bukit Purmei Road, Singapore 0904' in GoodCustomers table. */
 UPDATE GoodCustomers
 SET Address = '22 Bukit Purmei Road, Singapore 0904'
 WHERE CustomerName = 'Grace Leong'
 
--- Question 9
+/* 9. Change the Member Category to ‘B’ for customer whose Customer ID is 5108 in GoodCustomers table. */
 UPDATE GoodCustomers
 SET MemberCategory = 'B'
-WHERE CustomerName IN
-	(SELECT CustomerName
-	FROM Customers
-	WHERE CustomerID = 5108)
+WHERE CustomerName in (SELECT CustomerName FROM Customers
+						WHERE CustomerID = 5108)
 
--- Question 10
+/* 10. Remove Grace Leong from GoodCustomers table. */
 DELETE FROM GoodCustomers
 WHERE CustomerName = 'Grace Leong'
 
--- Question 11
+/* 11. Remove customers with ‘B’ member category in GoodCustomers table. */
 DELETE FROM GoodCustomers
 WHERE MemberCategory = 'B'
 
--- Question 12
+/* 12. Add column FaxNumber (nvarchar(25)) to GoodCustomers table. */
 ALTER TABLE GoodCustomers
 ADD FaxNumber nvarchar(25)
 
--- Question 13
+/* 13. Alter the column Address to nvarchar(80) in GoodCustomers table. */
 ALTER TABLE GoodCustomers
 ALTER COLUMN Address nvarchar(80)
 
--- Question 14
+/* 14. Add column ICNumber (nvarchar(10)) to GoodCustomers table. */
 ALTER TABLE GoodCustomers
 ADD ICNumber nvarchar(10)
 
--- Question 15
-CREATE UNIQUE INDEX ICIndex
-ON GoodCustomers(ICNumber)
-/* Can't create because duplicate keys found */
+/* 15. Create a unique index ICIndex on table GoodCustomers bases on ICNumber. Notice that the column ICNumber have no values. Can you create the unique index successfully? Why? */
+CREATE UNIQUE INDEX gdCust_idx
+ON GoodCustomers(PhoneNumber)
 
--- Question 16
-CREATE INDEX FaxIndex
+/* 16. Create an index on table GoodCustomers based on FaxNumber. */
+CREATE INDEX gdCust_idx
 ON GoodCustomers(FaxNumber)
 
--- Question 17
-DROP INDEX FaxIndex ON GoodCustomers
+/* 17. Drop the index created on FaxNumber. */
+DROP INDEX gdCust_idx ON GoodCustomers
 
--- Question 18
+/* 18. Remove the column FaxNumber from GoodCustomer table. */
 ALTER TABLE GoodCustomers
 DROP COLUMN FaxNumber
 
--- Question 19
+/* 19. Delete all records from GoodCustomers. */
 DELETE FROM GoodCustomers
 
--- Question 20
+/* 20. Drop the table GoodCustomers. */
 DROP TABLE GoodCustomers
-
--- For Previewing
-SELECT * FROM MemberCategories
-SELECT * FROM GoodCustomers
